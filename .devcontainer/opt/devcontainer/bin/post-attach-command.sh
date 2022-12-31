1a7bf70e83467e73fe21b79ba53ab86345d65f1d
ptaylor <paul.e.taylor@me.com>
2022-12-30 17:24:00 -0800

    open workspace in post-start-command.sh

--- .devcontainer/opt/devcontainer/bin/post-attach-command.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-for cmd in $(find /opt -type f -name post-attach-command.sh ! -wholename $0); do
-    . $cmd;
-done
-
-/opt/devcontainer/bin/open-vscode-workspace.sh;
--- .devcontainer/opt/devcontainer/bin/post-start-command.sh
+++ .devcontainer/opt/devcontainer/bin/post-start-command.sh
@@ -5,3 +5,5 @@
 for cmd in $(find /opt -type f -name post-start-command.sh ! -wholename $0); do
     . $cmd;
 done
+
+/opt/devcontainer/bin/open-vscode-workspace.sh;
