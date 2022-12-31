d362d72c497e2cb2335d3bafbc7c874a083f6a5a
ptaylor <paul.e.taylor@me.com>
2022-12-30 16:55:30 -0800

    clone legate in update-content-command.sh for codespaces

--- .devcontainer/opt/legate/bin/post-start-command.sh
+++ .devcontainer/opt/legate/bin/post-start-command.sh
@@ -1,5 +1,3 @@
 #! /usr/bin/env bash
 
 /opt/legate/bin/clone-legion.sh;
-/opt/legate/bin/clone-legate-core.sh;
-/opt/legate/bin/make-legate-test-env.sh;
---
+++ .devcontainer/opt/legate/bin/update-content-command.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env bash
+
+/opt/legate/bin/clone-legate-core.sh;
+/opt/legate/bin/make-legate-test-env.sh;
