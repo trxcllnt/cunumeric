baccbfe2eb70588cc2284f16fae74a4daaf5f8b8
ptaylor <paul.e.taylor@me.com>
2022-12-30 17:31:17 -0800

    only attempt to read if stdin is a tty

--- .devcontainer/opt/devcontainer/bin/init-git.sh
+++ .devcontainer/opt/devcontainer/bin/init-git.sh
@@ -8,7 +8,7 @@ if [[ -z "$(git config --get user.name)" ]]; then
     . /opt/devcontainer/bin/init-github-cli.sh || exit $?;
     git_user_name="$(gh api user --jq '.name')";
     if [[ $? != 0 ]]; then git_user_name=""; fi;
-    if [[ -z $git_user_name ]]; then
+    if [[ -z $git_user_name && -t 0 ]]; then
         read -p "Git user.name: " git_user_name </dev/tty;
     fi
     git config --global user.name "${git_user_name:-anon}";
@@ -18,7 +18,7 @@ if [[ -z "$(git config --get user.email)" ]]; then
     . /opt/devcontainer/bin/init-github-cli.sh || exit $?;
     git_user_email="$(gh api user/emails --jq '. | map(select(.primary == true)) | map(.email)[]')";
     if [[ $? != 0 ]]; then git_user_email=""; fi;
-    if [[ -z $git_user_email ]]; then
+    if [[ -z $git_user_email && -t 0 ]]; then
         read -p "Git user.email: " git_user_email </dev/tty;
     fi
     git config --global user.email "${git_user_email:-users.noreply.github.com}";
